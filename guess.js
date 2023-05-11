a28e3bd795b9c4ef5af475640ba156950f378438
kiruiaaron <76702806+kiruiaaron@users.noreply.github.com>
2023-05-11 12:17:25 +0300

    Update guess.js

--- guess.js
+++ guess.js
@@ -2,8 +2,8 @@ let num = 7;
 
 let i = prompt("Guess a number between 1 and 10");
 
-if(i<0 || i>10){
-    break;}
+if(i<1 || i>10){
+   console.log("you entered wrong number");}
  else if(i == num){
     alert("Good work");
   
